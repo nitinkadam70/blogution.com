381b287241ccc6d05b8c8d694172716132199f66
Nitin Kadam <99539536+nitinkadam70@users.noreply.github.com>
2023-04-18 14:41:45 +0000

    setup db

---
+++ blog-backend/config/db.js
@@ -0,0 +1,7 @@
+const { connect, default: mongoose } = require('mongoose');
+require('dotenv').config();
+
+const MongoDB_URL = process.env.MONGODB_URL;
+const connection = mongoose.connect(MongoDB_URL);
+
+module.exports = { connection };
