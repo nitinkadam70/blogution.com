e9bf5bf4b676a9cddae4e6f03a269e453c6c6929
Shivaji z <99667776+CodEsHiVaz@users.noreply.github.com>
2023-04-26 21:49:52 +0530

    xxxx

--- blog-backend/routes/blogs.route.js
+++ blog-backend/routes/blogs.route.js
@@ -1,22 +1,19 @@
-const { Router } = require("express");
-
+const { Router } = require('express');
 const {
   getBlogs,
   createBlog,
   updateBlog,
   getSingleBlogs,
   deleteBlog,
-  login
-} = require("../controllers/blogs.controller");
-const { isAuth } = require("../middlewares/auth");
+  filterBlogs,
+} = require('../controllers/blogs.controller');
 
 const blogsRouter = Router();
 
-blogsRouter.get("/posts", getBlogs);
-blogsRouter.get("/posts/:id", getSingleBlogs);
-blogsRouter.post("/posts/create",isAuth, createBlog);
-blogsRouter.patch("/posts/update/:id",isAuth, updateBlog);
-blogsRouter.delete("/posts/delete/:id",isAuth, deleteBlog);
-blogsRouter.post("/auth/login", login);
+blogsRouter.get('/posts', getBlogs);
+blogsRouter.get('/posts/:id', getSingleBlogs);
+blogsRouter.post('/posts/create', createBlog);
+blogsRouter.patch('/posts/:id', updateBlog);
+blogsRouter.delete('/posts/:id', deleteBlog);
 
 module.exports = blogsRouter;
